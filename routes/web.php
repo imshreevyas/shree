58929b07f796c045b09457b23aa65121439ba9cc
shrikunjvyaspinkvilla <shrikunj.vyas@pinkvilla.com>
2023-06-03 11:22:09 +0530

    Created Frontend Controller and View

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+
+// Admin
+
+
+// Users
+Route::prefix('/')->group(function () {
+    
+    Route::get('/home', [FrontendController::class, 'index']);
+
+
+    
 });
